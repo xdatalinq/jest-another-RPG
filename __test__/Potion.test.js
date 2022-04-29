0f4c197128f79cd7e93a61c6f7d4886c80bd90a8
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-04-29 16:47:56 -0400

    almost done player object

--- __test__/Potion.test.js
+++
@@ -1,9 +0,0 @@
-const Potion = require('../lib/Potion.js');
-
-test('creates a health potion object', () => {
-    const potion = new Potion();
-
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-    expect(potion.value).toEqual(expect.any(Number));
-});
